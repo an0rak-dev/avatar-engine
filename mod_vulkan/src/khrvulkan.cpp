42f4f2e6773e89c04ef093a25bcb14cbc5c60b7d
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2023-05-10 23:33:55 +0200

    Choose the appropriate device

--- mod_vulkan/src/khrvulkan.cpp
+++ mod_vulkan/src/khrvulkan.cpp
@@ -1,12 +1,16 @@
 #include <khrvulkan.hpp>
 #include <stdlib.h>
+#include <stdio.h>
 #include <vulkan/vulkan.h>
 
-#include <mod_vulkan/extensions.hpp>
+#include <mod_vulkan/extensions.hpp> 
+
+#define MAX_DEVICES_COUNT 16
 #define MODULE_VERSION 1
 
 struct vulkan_specifics {
-	VkInstance instance;
+	VkInstance       instance;
+	VkPhysicalDevice device;
 };
 
 int vulkan_allocate(av_vulkan* out_vulkan) {
@@ -14,6 +18,8 @@ int vulkan_allocate(av_vulkan* out_vulkan) {
 	if (NULL == allocated_specifics) {
 		return 1;
 	}
+	allocated_specifics->instance = VK_NULL_HANDLE;
+	allocated_specifics->device = VK_NULL_HANDLE;
 	out_vulkan->specifics = allocated_specifics;
 	return 0;
 }
@@ -27,6 +33,7 @@ void vulkan_destroy(av_vulkan* inout_vulkan) {
 }
 
 int vulkan_initialize(av_vulkan& vulkan, enum av_supported_surface surface_kind) {
+	// Instance
 	VkApplicationInfo application_info = { VK_STRUCTURE_TYPE_APPLICATION_INFO };
 	VkInstanceCreateInfo create_info   = { VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
 	av_modvulkan_extensions extensions = {};
@@ -61,5 +68,29 @@ int vulkan_initialize(av_vulkan& vulkan, enum av_supported_surface surface_kind)
 		// TODO Error
 		return 2;
 	}
+
+	// Device
+	unsigned int devices_count = 0;
+	VkPhysicalDevice devices[MAX_DEVICES_COUNT];
+	if (VK_SUCCESS != vkEnumeratePhysicalDevices(vulkan.specifics->instance, &devices_count, NULL)) {
+		return 3;
+	}
+	if (VK_SUCCESS != vkEnumeratePhysicalDevices(vulkan.specifics->instance, &devices_count, devices)) {
+		return 4;
+	}
+	for (int i = 0; i < devices_count; i++) {
+		VkPhysicalDeviceProperties properties = {};
+		vkGetPhysicalDeviceProperties(devices[i], &properties);
+		if (properties.apiVersion >= application_info.apiVersion) {
+			// TODO : Choose the device in a smarter way (the more VRAM, a high score of features...)
+			printf("Using %s graphic card\n", properties.deviceName);
+			vulkan.specifics->device = devices[i];
+			break;
+		}
+	}
+	if (VK_NULL_HANDLE == vulkan.specifics->device) {
+		return 5;
+	}
+
 	return 0;
 }
