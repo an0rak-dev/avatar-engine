1c8e578cb3cc0a64343c4d9cf4f409386b88ee06
Sylvain Nieuwlandt <nieuwlandt.s@an0rak.dev>
2023-07-14 00:49:01 +0200

    Adding some doc

--- includes/avatar/core/level.hpp
+++ includes/avatar/core/level.hpp
@@ -3,8 +3,17 @@
 
 #include <avatar/core/scene.hpp>
 
+/**
+ Defines the basic components of a single game level.
+
+ A game level represents an aggregation of structure to
+ describe a scene, the protagonists in it, as well as the
+ specific rules, if any.
+*/
 typedef struct level {
+		/* The scene containing all the objects to render on screen */
 		av_scene scene;
+
 } av_level;
 
 #endif
